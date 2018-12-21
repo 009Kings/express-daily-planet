23de785f6f4d381c9cb5886390b804ce1f20d2fb
009kings <009kings@gmail.com>
2018-12-21 15:12:20 -0800

    routes stubbed out with rendered pages

--- index.js
+++ index.js
@@ -11,5 +11,21 @@ app.get("/", (req, res)=>{
     res.render("home");
 })
 
+app.get('/articles', (req, res) => {
+    res.render("articles/index");
+});
+
+app.get('/articles/new', (req, res) => {
+    res.render("articles/new");
+});
+
+app.post('/articles', (req, res) => {
+    res.redirect("/articles");
+});
+
+app.get('/articles/:id', (req, res) => {
+    res.render("articles/show", {id: req.params.id});
+});
+
 // Listen
 app.listen("8000", ()=> console.log("You're listening to the smooth sounds of port 8000"));
